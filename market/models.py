2f193111fe871865f49da82a7bc173f2bed016d7
vijay12panwar <vijaychoudhary11202@gmail.com>
2025-01-12 14:38:01 +0530

    Implement item purchasing feature with balance validation

--- market/models.py
+++ market/models.py
@@ -38,6 +38,9 @@ class User(db.Model, UserMixin):
 
     def check_password_correction(self, attempted_password):
         return bcrypt.check_password_hash(self.password_hash, attempted_password)
+    
+    def can_purchase(self, item_price):
+        return self.budget >= item_price
 
 
 class Item(db.Model):
@@ -50,3 +53,9 @@ class Item(db.Model):
 
     def __repr__(self):
         return f'Item {self.name}'
+    
+    def assign_ownership(self, user):
+        self.owner = user.id
+        user.budget -= self.price
+        db.session.commit()
+
--- market/routes.py
+++ market/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
 from market import app, db
 from market.models import Item, User
-from market.forms import RegisterForm, LoginForm
-from flask_login import login_user, logout_user, login_required
+from market.forms import RegisterForm, LoginForm, PurchaseItemForm
+from flask_login import login_user, logout_user, login_required, current_user
 
 @app.route("/")
 @app.route("/home")
@@ -10,11 +10,23 @@ def home_page():
     return render_template("home.html")
 
 
-@app.route("/market")
+@app.route("/market", methods=['GET', 'POST'])
 @login_required
 def market_page():
-    items = Item.query.all()
-    return render_template("market.html", items=items)
+    purchase_form = PurchaseItemForm()
+    if request.method == "POST":
+        purchased_item = request.form.get('purchase_item')
+        p_item_object = Item.query.filter_by(name=purchased_item).first()
+        if p_item_object:
+            if current_user.can_purchase(item_price=p_item_object.price):
+                p_item_object.assign_ownership(user=current_user)
+                flash(f'Congratulations! You have successfully purchased {purchased_item} for ${p_item_object.price}', category='success')
+            else:
+                flash(f"Unfortunately, you don't have enough money to purchase {purchased_item}!", category='danger')
+        return redirect(url_for('market_page'))
+    else:
+        items = Item.query.filter_by(owner=None)
+        return render_template("market.html", items=items, purchase_form=purchase_form)
 
 
 @app.route("/register", methods=['GET', 'POST'])
